fa9e4d54eff9383bab8c20bb7b6160cd55f159d7
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2026-09-19 00:28:13 -0600

    Tenemos un poco de arte, hay que ver el multi local

--- Assets/Scripts/Controllers/UnitHealthbar.cs
+++ Assets/Scripts/Controllers/UnitHealthbar.cs
@@ -1,14 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UnitHealthbar : MonoBehaviour
 {
-    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Image heartFillImage;
+    [SerializeField] private float velocityImage;
+    [SerializeField] private float distanceImage;
+    
+    private RectTransform rectTransform;
+    private Vector2 position;
 
     private Unit _unit;
 
     private void Start()
     {
+        rectTransform = GetComponent<RectTransform>();
+        position = rectTransform.anchoredPosition;
+        
         _unit = GetComponentInParent<Unit>();
 
         if (_unit == null)
@@ -16,23 +25,39 @@ public class UnitHealthbar : MonoBehaviour
             Debug.LogError($"{name} no encontró una unidad padre." );
             return;
         }
+        
+        if (heartFillImage == null)
+        {
+            Debug.LogError($"{name} no hay un HeartFill." );
+            return;
+        }
+        
+        heartFillImage.type = Image.Type.Filled;
+        heartFillImage.fillMethod = Image.FillMethod.Vertical;
+        heartFillImage.fillOrigin = (int)Image.OriginVertical.Bottom;
+        heartFillImage.raycastTarget = false;
 
         _unit.StatsChanged += Refresh;
         Refresh();
     }
 
+    private void Update()
+    {
+        float newYPosition = position.y + Mathf.Sin(Time.time * velocityImage) * distanceImage;
+        rectTransform.anchoredPosition = new Vector2(position.x, newYPosition);
+    }
+
     private void Refresh()
     {
-        if (_unit == null) return;
-        if (healthSlider == null) return;
-
+        if (_unit == null  || heartFillImage == null) return;
         if (_unit.MaxHealth <= 0)
         {
-            healthSlider.value = 0f;
+            heartFillImage.fillAmount = 0f;
             return;
         }
 
-        healthSlider.value = (float)_unit.CurrentHealth / _unit.MaxHealth;
+        float healthPercentage = (float)_unit.CurrentHealth / _unit.MaxHealth;
+        heartFillImage.fillAmount = Mathf.Clamp01(healthPercentage);
     }
 
     private void OnDestroy()
